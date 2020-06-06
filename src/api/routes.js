6a27e4d417152aae0f9601bef9488d0f4b8f24f0
evandro_gdn <evandrogardolinn@gmail.com>
2020-06-06 15:16:38 -0300

    [ADD] #routes - Adicionadas rotas referentes as operacoes de cadastro de uma conta cliente

--- src/api/routes.js
+++ src/api/routes.js
@@ -1,8 +1,19 @@
 const { Router } = require('express');
+const { ContaCliente } = require('./../controllers')
 
 class Routes {
   constructor() {
     this.routes = new Router();
+    // carregamento de rotas
+    this.loadRotasContaCliente();
+  }
+
+  loadRotasContaCliente() {
+    this.routes.get('conta-cliente', ContaCliente.index);
+    this.routes.get('conta-cliente/:id', ContaCliente.getOne);
+    this.routes.post('conta-cliente', ContaCliente.create);
+    this.routes.put('conta-cliente/:id', ContaCliente.update);
+    this.routes.delete('conta-cliente/:id', ContaCliente.remove)
   }
 }
 
